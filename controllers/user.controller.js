7c2d9619cb202be464088adca25225ab6e256407
sid-1974 <sidgithub1974@gmail.com>
2025-03-15 10:40:21 +0530

    removed  console

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,6 @@ const userGetDetails = async(req,res)=>{
         const user = await UserModel.findOne({ username: req.params.userid });
         res.status(200).json({success: true,user});
     } catch (error) {
-        console.error("Error fetching user details:", error);
         res.status(500).json({success:false, message: error });
     }
 }
