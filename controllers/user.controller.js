7c35844c58781f391b16418d6a12cc7bc3b55157
sid-1974 <sidgithub1974@gmail.com>
2025-03-25 15:02:58 +0530

    Added contact page

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 const UserModel = require("../models/user.model");
+const { sendMail } = require("../utils/EmailService");
+require("dotenv").config();
 
 
 const userGetDetails = async(req,res)=>{
@@ -42,4 +44,26 @@ const userUpdateDetails = async(req,res)=>{
     }
 }
 
-module.exports ={ userGetDetails,getAllusers,userUpdateDetails}
+const ContactPage = async(req,res)=>{
+    try {
+        const { name, email, phone, message } = req.body;
+        const adminEmail = process.env.EMAIL_USER;
+        const subject = `New contact inquiry from ${name}`; 
+        const emailBody= `New contact inquiry received:\n\n Name: ${name}\n Email: ${email}\n Phone: ${phone} \n Message: ${message}\n\n `
+        await sendMail(adminEmail, subject, emailBody);
+    
+        const userSubject = 'Thank you for contacting us';
+        const userBody = ` Dear ${name},\n\nThank you for reaching out to us.\nWe have received your message and will get back to you shortly.\n\nBest regards,\nSK Properties Team `
+        await sendMail(email, userSubject, userBody);
+        res.status(200).json({ 
+            success: true, 
+            message: 'Your message has been sent successfully' 
+          });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+   
+}
+
+
+module.exports ={ userGetDetails,getAllusers,userUpdateDetails,ContactPage}
